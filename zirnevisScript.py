c451fbd816f8d2cdf8f33326194f77edd0a398ab
Younes <younesvatan78@gmail.com>
2020-12-12 20:58:28 +0330

    host changed

--- zirnevisScript.py
+++ zirnevisScript.py
@@ -14,6 +14,7 @@ instagram: kia.hamediii
 import requests
 from bs4 import BeautifulSoup
 from os import system
+from os import path
 
 movie = str(input('Enter Movie (Example Avengers Endgame):'))
 formatt = str(input('Enter Format (Example WEB-DL):'))
@@ -21,23 +22,25 @@ res = str(input('Enter Res (Example 720):'))
 
 
 
-baseUrl = "https://mrsub.net"
+baseUrl = "http://subf2m.ir"
 nameMovie = movie.replace(" ","+")
 
-searchurl = baseUrl+"/subtitles/searchbytitle?query="+str(nameMovie)
+searchurl = baseUrl+"/subtitles/زیرنویس-فارسی-searchbytitle?query="+str(nameMovie)
 searchPage = requests.get(searchurl)
 soup = BeautifulSoup(searchPage.content,'html5lib')
 div = soup.find('div',{"class":"title"})
-namesArray = nameMovie.split("+")
 
+namesArray = nameMovie.split("+")
 searchNameFound = div.text.strip()
+#print(searchNameFound)
+#print(namesArray)
 
 for check in namesArray:
     if check not in searchNameFound:
         print("not exist this movie")
         exit(0)
         
-urlMovie = baseUrl + div.find('a')['href']+"/farsi_persian/"
+urlMovie = baseUrl + div.find('a')['href']
 moviePage = requests.get(urlMovie)
 
 
@@ -47,11 +50,12 @@ for item in soup.find_all('li',{"class":"item"}):
     for name in item.find_all('ul',{"class":"scrolllist"}):
         if formatt in name.text.strip() and res in name.text.strip():
             urlDownloadPage = baseUrl + item.find('a',{"class":"download icon-download"})['href']
-            print(urlDownloadPage)
+            #print(urlDownloadPage)
             downloadPage = requests.get(urlDownloadPage)
             soup = BeautifulSoup(downloadPage.content,'html5lib')
             aURLDOWNLOAD = soup.find('a',{"id":"downloadButton"})['href']
-            system("wget -O "+str(counter)+".zip '"+aURLDOWNLOAD+"'")
+            print(aURLDOWNLOAD)
+            system("wget -O "+str(counter)+".zip '"+baseUrl+""+aURLDOWNLOAD+"'")
             system("unzip "+str(counter)+".zip && rm "+str(counter)+".zip")
             counter += 1
             
@@ -60,4 +64,8 @@ for item in soup.find_all('li',{"class":"item"}):
         
 # Move to Folder
 MovieDirectory = movie + ' subtitles'
+#if not path.isdir(MovieDirectory):
+
 system("mkdir '" + MovieDirectory + "' && mv *.srt '" + MovieDirectory + "'")
+#else:
+ #   system("mv *.srt '" + MovieDirectory + "'")
